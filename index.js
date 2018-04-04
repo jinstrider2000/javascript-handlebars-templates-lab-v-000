5a6702b1b0f1af26232352ac19a75af450412d2f
Efrain Perez Jr <efrainperezjr@live.com>
2018-04-04 22:13:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
 function createRecipe() {
-  var template = Handlebars.compile(document.getElementById('recipe-template'));
+  var template = Handlebars.compile(document.getElementById('recipe-template').innerHTML);
 }
 
 function displayEditForm() {
-  var template = Handlebars.compile(document.getElementById("recipe-form-template"));
+  var template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
 }
 
 function updateRecipe() {
-  var template = Handlebars.compile(document.getElementById('recipe-template'));
+  var template = Handlebars.compile(document.getElementById('recipe-template').innerHTML);
 }
