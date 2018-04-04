f54aa58d355200a67f0f212f795c4989a7e44c11
Efrain Perez Jr <efrainperezjr@live.com>
2018-04-04 21:16:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function init() {
   //put any page initialization/handlebars initialization here
-  Handlebars.registerPartial('recipeDetailsPartial')
+  Handlebars.registerPartial('recipeDetailsPartial',document.getElementById('recipe-details-partial').innerHTML);
+  Handlebars.registerHelper('displayIngredient', (ingredient) => {
+    return `${ingredient.name}`;
+  });
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
